cd44fd050cb848be7eadcc8b4b4e6048435d883b
shukina7olga <olga.shchukina.99@mail.ru>
2020-07-05 03:16:35 +0300

    кнопки закртытия-открытия меню

--- script/script.js
+++ script/script.js
@@ -55,4 +55,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }
     countTimer('04 july 2020');
+
+    //Burger-menu
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+            if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+                menu.style.transform = `translate(0)`;
+            } else {
+                menu.style.transform = `translate(-100%)`;
+            }
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+    };
+    toggleMenu();
 });
